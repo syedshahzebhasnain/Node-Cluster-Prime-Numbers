d3885872381b9abecf5b05af12cbb6b8719ebe3a
shasnain <hasnain.shahzeb@gmail.com>
2020-05-23 15:50:05 -0300

    lint solution

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ require('dotenv').config()
 // Initialize cluster
 var cluster = require('cluster')
 const numCPU = require('os').cpus().length
-   
 
 if (cluster.isWorker) {
-    // Load prime library
-    const primeCalc = require('./primeDetails.js')
-    primeCalc.GetPrimeAndWriteToFile(process.env.start, process.env.end)
-    process.exit()
+  // Load prime library
+  const primeCalc = require('./primeDetails.js')
+  primeCalc.GetPrimeAndWriteToFile(process.env.start, process.env.end)
+  process.exit()
 } else {
-    // Ensuring numbers are always a multiple of 2
-    // Split into workers based on the max number
-    if (maxNumber % 2 === 1) maxNumber++ 
-        const bracket = maxNumber / numCPU
-    for (let i = 0; i < numCPU; i++) {
-        let multiplier = i
-        var envPass = { start: bracket * multiplier, end: (bracket * ++multiplier) - 1 }
-        cluster.fork(envPass)
-    }
+  // Ensuring numbers are always a multiple of 2
+  // Split into workers based on the max number
+  if (maxNumber % 2 === 1) maxNumber++
+  const bracket = maxNumber / numCPU
+  for (let i = 0; i < numCPU; i++) {
+    let multiplier = i
+    var envPass = { start: bracket * multiplier, end: (bracket * ++multiplier) - 1 }
+    cluster.fork(envPass)
+  }
 }
--- primeDetails.js
+++ primeDetails.js
@@ -4,54 +4,53 @@ const fs = require('fs-extra')
 
 module.exports = {
 
-    // Main function to start the process.
-    async GetPrimeAndWriteToFile(start, end) {
-        try {
-            var startTime = new Date().getTime();
-            let primeNumDetails = []
-            for (let i = +start; i <= +end; i++) {
-                if (this.IsPrime(i) === true) {
-                    primeNumDetails.push(this.GenerateHashes(i))
-                }
-            }
-            this.WriteTofile(primeNumDetails);
-            this.CheckExecutionTime(startTime, start, end)
-        } catch (err) {
-            console.log(err)
+  // Main function to start the process.
+  async GetPrimeAndWriteToFile (start, end) {
+    try {
+      var startTime = new Date().getTime()
+      const primeNumDetails = []
+      for (let i = +start; i <= +end; i++) {
+        if (this.IsPrime(i) === true) {
+          primeNumDetails.push(this.GenerateHashes(i))
         }
-    },
-
-    // Generate hashes for the provided number
-    GenerateHashes(i) {
-        let sstring = i.toString()
-        let hashes = {
-            Number: i,
-            sha1: CryptoJS.SHA1(sstring).toString().replace(/\D/g, '').toString(),
-            sha2: CryptoJS.SHA3(sstring).toString().replace(/\D/g, '').toString(),
-            sha256: CryptoJS.SHA256(sstring).toString().replace(/\D/g, '').toString(),
-            sha384: CryptoJS.SHA384(sstring).toString().replace(/\D/g, '').toString(),
-            sha512: CryptoJS.SHA512(sstring).toString().replace(/\D/g, '').toString(),
-            md5: CryptoJS.MD5(sstring).toString().replace(/\D/g, '').toString()
-        }
-        return hashes;
-    },
-
-    // Check for prime number
-    IsPrime(num) {
-        for (let i = 2, s = Math.sqrt(num); i <= s; i++)
-            if (num % i === 0) return false;
-        return num !== 1 && num !== 0;
-    },
-
-    // Write to file
-    WriteTofile(primeNumDetails) {
-        fs.writeFileSync(process.env.start + '.txt', JSON.stringify(primeNumDetails))
-    },
-
-    // Check execution time
-    CheckExecutionTime(startTime, startNumber, endNumber) {
-        var endTime = new Date().getTime();
-        var time = endTime - startTime;
-        console.log('Execution time: ' + startNumber + '  --  ' + endNumber + ': ' + time);
+      }
+      this.WriteTofile(primeNumDetails)
+      this.CheckExecutionTime(startTime, start, end)
+    } catch (err) {
+      console.log(err)
     }
+  },
+
+  // Generate hashes for the provided number
+  GenerateHashes (i) {
+    const sstring = i.toString()
+    const hashes = {
+      Number: i,
+      sha1: CryptoJS.SHA1(sstring).toString().replace(/\D/g, '').toString(),
+      sha2: CryptoJS.SHA3(sstring).toString().replace(/\D/g, '').toString(),
+      sha256: CryptoJS.SHA256(sstring).toString().replace(/\D/g, '').toString(),
+      sha384: CryptoJS.SHA384(sstring).toString().replace(/\D/g, '').toString(),
+      sha512: CryptoJS.SHA512(sstring).toString().replace(/\D/g, '').toString(),
+      md5: CryptoJS.MD5(sstring).toString().replace(/\D/g, '').toString()
+    }
+    return hashes
+  },
+
+  // Check for prime number
+  IsPrime (num) {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++) { if (num % i === 0) return false }
+    return num !== 1 && num !== 0
+  },
+
+  // Write to file
+  WriteTofile (primeNumDetails) {
+    fs.writeFileSync(process.env.start + '.txt', JSON.stringify(primeNumDetails))
+  },
+
+  // Check execution time
+  CheckExecutionTime (startTime, startNumber, endNumber) {
+    var endTime = new Date().getTime()
+    var time = endTime - startTime
+    console.log('Execution time: ' + startNumber + '  --  ' + endNumber + ': ' + time)
+  }
 }
